3cd7d2009c635fb9457816af6f45eb2661d89a7b
Buran Udachan <buran.udachan@itensia.com>
2022-03-12 10:33:22 +0530

    Auth setup done

---
+++ server/server.py
@@ -0,0 +1,41 @@
+from crypt import methods
+from flask import Flask, jsonify, request, abort
+from flask_bcrypt import Bcrypt
+from models import db, User
+from config import ApplicationConfig
+
+app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
+
+bcrypt = Bcrypt(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/register", methods=["POST"])
+def register_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user_exists = User.query.filter_by(email=email).first() is not None
+
+    if user_exists:
+        return jsonify({
+            "error": "user already exists"
+        })
+
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email,
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
